22a685755fcc5af755abf0e5120356df198f8fcb
enrico bianco <biancoenrico71@gmail.com>
2021-04-09 18:36:23 +0200

    esercitazione completata

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* Riproducete il menu con anche le dropdown funzionanti come da screenshot.
+Il menu deve funzionare su desktop.
+Utilizzate jQuery per mostrare e nascondere le dropdown.*/
+
+
+
+$('nav>ul>li').mouseenter(function () {
+
+    $(this).next().addClass('relative')
+    
+    if($(this).children() == $('button')){
+        !$(this).children().addClass('show');
+    }else{
+        $(this).children().addClass('show');
+    }
+})
+$('nav>ul>li').mouseleave(function () {
+    $(this).next().removeClass('relative')
+    $(this).children().removeClass('show');
+
+})
